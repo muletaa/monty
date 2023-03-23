f148d50ee5900870dc3d47ae98479b216446791c
muletaa <muleta.ketela@yahoo.com>
2023-03-23 04:52:44 -0700

    team project

--- push.c
+++ push.c
@@ -19,14 +19,10 @@ void f_push(stack_t **head, unsigned int counter)
 				flag = 1; }
 		if (flag == 1)
 		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-			fclose(bus.file);
-			free(bus.content);
 			free_stack(*head);
 			exit(EXIT_FAILURE); }}
 	else
 	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-		fclose(bus.file);
-		free(bus.content);
 		free_stack(*head);
 		exit(EXIT_FAILURE); }
 	n = atoi(bus.arg);
